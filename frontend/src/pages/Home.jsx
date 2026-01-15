3d9b0db67c3f84ef65c35c320cd09c430270878f
Sachin Gajanan Giri <sachinggiri01@gmail.com>
2026-01-15 20:25:50 +0530

    Create the OfferPage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import BannerHome from "../components/BannerHome";
 import CategoriesHome from "../components/CategoriesHome";
 import ComingSoonWatchesPage from "../components/ComingSoonWatchesPage";
+import FashionPage from "../components/FashionPage";
 const Home=()=>{
   return (
     <div>
       <BannerHome/>
 <CategoriesHome/>
 <ComingSoonWatchesPage/>
+<FashionPage/>
     </div>
   )
 }
